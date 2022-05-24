a3f72b003e2a9880c878e6a4d6a47af8a952805c
RafatAiub <tanvirmahtab1996@gmail.com>
2022-05-24 09:36:00 +0600

    routes set with different pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
+import About from './Pages/About/About';
+import Order from './Pages/Order/Order';
+import Contact from './Pages/Contact/Contact';
+import Review from './Pages/Order/Review';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order" element={<Order />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="review" element={<Review />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
